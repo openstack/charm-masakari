b0ad40223c2166d56dae7b0ce44011506dd7273d
Liam Young <liam.young@canonical.com>
2019-02-18 15:32:57 +0000

    Fix keystone service registry and host api service in mod_wsgi

--- src/lib/charm/openstack/masakari.py
+++ src/lib/charm/openstack/masakari.py
@@ -1,14 +1,20 @@
 import collections
 import socket
 import subprocess
+import tempfile
 
 import charmhelpers.core.hookenv as hookenv
 import charms_openstack.charm
 import charms_openstack.ip as os_ip
+import charmhelpers.contrib.openstack.utils as ch_os_utils
+
 
 # import charms_openstack.sdn.odl as odl
 # import charms_openstack.sdn.ovs as ovs
 
+MASAKARI_WSGI_CONF = '/etc/apache2/sites-enabled/masakari-api.conf'
+
+charms_openstack.charm.use_defaults('charm.default-select-release')
 
 class MasakariCharm(charms_openstack.charm.HAOpenStackCharm):
 
@@ -19,7 +25,7 @@ class MasakariCharm(charms_openstack.charm.HAOpenStackCharm):
     release = 'mitaka'
 
     # List of packages to install for this charm
-    packages = ['masakari', 'python-apt']
+    packages = ['libapache2-mod-wsgi', 'apache2', 'python-apt', 'cinder-common']
 
     api_ports = {
         'masakari': {
@@ -31,19 +37,18 @@ class MasakariCharm(charms_openstack.charm.HAOpenStackCharm):
 
     service_type = 'masakari'
     default_service = 'masakari'
-    services = ['haproxy', 'masakari']
+    services = ['haproxy', 'apache2', 'masakari-engine']
 
-    # Note that the hsm interface is optional - defined in config.yaml
     required_relations = ['shared-db', 'amqp', 'identity-service']
 
     restart_map = {
-
         '/etc/masakari/masakari.conf': services,
+        MASAKARI_WSGI_CONF: services,
     }
 
-    ha_resources = ['vips', 'haproxy']
+    ha_resources = ['vips', 'haproxy', 'dnsha']
 
-    release_pkg = 'masakari-common'
+    release_pkg = 'cinder-common'
 
     package_codenames = {
         'masakari-common': collections.OrderedDict([
@@ -54,7 +59,7 @@ class MasakariCharm(charms_openstack.charm.HAOpenStackCharm):
     }
 
 
-    sync_cmd = ['congress-db-manage', '--config-file', '/etc/congress/congress.conf', 'upgrade', 'head']
+    sync_cmd = ['masakari-manage', '--config-file', '/etc/masakari/masakari.conf', 'db', 'sync']
 
     def get_amqp_credentials(self):
         return ('masakari', 'masakari')
@@ -62,5 +67,33 @@ class MasakariCharm(charms_openstack.charm.HAOpenStackCharm):
     def get_database_setup(self):
         return [{
             'database': 'masakari',
-            'username': 'masakari',
-            'hostname': hookenv.unit_private_ip() },]
+            'username': 'masakari'}]
+
+    @property
+    def public_url(self):
+        return super().public_url + "/v1/%(tenant_id)s"
+
+    @property
+    def admin_url(self):
+        return super().admin_url + "/v1/%(tenant_id)s"
+
+    @property
+    def internal_url(self):
+        return super().internal_url + "/v1/%(tenant_id)s"
+
+    # XXX THIS IS A TEMPORARY WORKAROUND AND SHOULD NOT BE INCLUDED IN
+    # ANY DEPLOYMENTS OTHER THAN POCs
+    def install(self):
+        super(MasakariCharm, self).install()
+        os_release = ch_os_utils.get_os_codename_package('cinder-common')
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            git_dir = '{}/masakari'.format(tmpdirname)
+            subprocess.check_call([
+                'git', 'clone', '-b', 'stable/{}'.format(os_release),
+                'https://github.com/openstack/masakari.git', git_dir])
+            subprocess.check_call([
+                'sudo', 'python', 'setup.py', 'install'], cwd=git_dir)
+        subprocess.check_call(['mkdir', '-p', '/var/lock/masakari', '/var/log/masakari', '/var/lib/masakari'])
+        subprocess.check_call(['cp', 'templates/masakari-engine.service', '/lib/systemd/system'])
+        subprocess.check_call(['systemctl', 'daemon-reload'])
+        subprocess.check_call(['systemctl', 'start', 'masakari-engine'])
--- src/reactive/masakari_handlers.py
+++ src/reactive/masakari_handlers.py
@@ -36,7 +36,7 @@ def render_config(*args):
     available.
     """
     with charm.provide_charm_instance() as charm_class:
-        charm_class.upgrade_if_available(args)
+#        charm_class.upgrade_if_available(args)
         charm_class.render_with_interfaces(args)
         charm_class.assess_status()
     reactive.set_state('config.rendered')
